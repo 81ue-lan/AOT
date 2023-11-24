e0d2e7fcfcae2e6cc32e5ce978a318563b271123
kitty <kitty90508s@gmail.com>
2023-11-24 13:38:31 +0800

    名字動畫

--- src/components/BaseSlider.vue
+++ src/components/BaseSlider.vue
@@ -9,7 +9,7 @@ const sliderList = [
         japaneseName: 'エレン・イェーガー',
         height: '170cm',
         weight: '63kg',
-        birth: '835-3-30',
+        birth: '835/3/30',
         image: 'eren.png'
     },
     {
@@ -17,7 +17,7 @@ const sliderList = [
         japaneseName: 'ミカサ・アッカーマン',
         height: '160cm',
         weight: '50kg',
-        birth: '835-2-10',
+        birth: '835/2/10',
         image: 'mikasa.png'
     },
     {
@@ -25,7 +25,7 @@ const sliderList = [
         japaneseName: 'コニー・スプリンガー',
         height: '158cm',
         weight: '58kg',
-        birth: '835-5-2',
+        birth: '835/5/2',
         image: 'connie.png'
     },
     {
@@ -33,7 +33,7 @@ const sliderList = [
         japaneseName: 'ハンジ・ゾエ',
         height: '170cm',
         weight: '60kg',
-        birth: '9-5',
+        birth: '9/5',
         image: 'hans.png'
     },
     {
@@ -41,7 +41,7 @@ const sliderList = [
         japaneseName: 'リヴァイ',
         height: '160cm',
         weight: '65kg',
-        birth: '12-25',
+        birth: '12/25',
         image: 'levi.png'
     },
     {
@@ -49,7 +49,7 @@ const sliderList = [
         japaneseName: 'エルヴィン・スミス',
         height: '188cm',
         weight: '92kg',
-        birth: '10-14',
+        birth: '10/14',
         image: 'erwin.png'
     },  
     {
@@ -57,7 +57,7 @@ const sliderList = [
         japaneseName: 'サシャ・ブラウス',
         height: '168cm',
         weight: '55kg',
-        birth: '7-26',
+        birth: '7/26',
         image: 'sasha.png'
     },
     {
@@ -65,7 +65,7 @@ const sliderList = [
         japaneseName: 'ジャン・キルシュタイン',
         height: '175cm',
         weight: '65kg',
-        birth: '835-4-7',
+        birth: '835/4/7',
         image: 'jean.png'
     },
     {
@@ -73,7 +73,7 @@ const sliderList = [
         japaneseName: 'アルミン・アルレルト',
         height: '163cm',
         weight: '55kg',
-        birth: '835-11-3',
+        birth: '835/11/3',
         image: 'armin.png'
     },
 ]
@@ -82,9 +82,9 @@ const slideItemWidth = ref(0)
 const offset = ref(0)
 const slideList = ref('')
 const slideItem = ref('')
-const isFlip = ref(false)
-const pre = ref('')
-const active = ref('')
+const isUpdate = ref(false)
+const clickWait = ref(false)
+const timer = ref({})
 const { width: screenWidth } = useWindowSize()
 
 const cloneSlide = computed(() => {
@@ -102,6 +102,16 @@ const preContent = computed(() => {
     const preData = filterData(preSlide)
     return preData
 })
+
+//阻擋連續點擊
+const setTime = () => {
+  timer.value = setTimeout(() => {
+    clickWait.value = false
+  }, 500)
+}
+const stopTime = () =>  {
+  clearInterval(timer.value)
+}
 
 const calculateOffset = () => {
   offset.value = -(
@@ -135,32 +145,83 @@ const init = () => {
     calculateOffset()
 }
 const moveSlide = (direction) => {
+    if(clickWait.value){
+        return
+    }
+    stopTime()
+    clickWait.value = true
     activeIndex.value += direction > 0 ? 1 : -1
+    setTime()
     offset.value += -direction * slideItemWidth.value
-    isFlip.value = true
+    isUpdate.value = true
+
+    animate()
     slideList.value.style.transform = `translateX(${ offset.value }px)`
     slideList.value.style.transition = 'all 0.5s ease-in-out'
 }
 const updateSliderPosition = () => {
     slideList.value.style.transition = 'none'
+    
     if (activeIndex.value > sliderList.length + 2) {
         activeIndex.value = 3
     }
     if (activeIndex.value < 3) {
         activeIndex.value = sliderList.length + 2
     }
-    isFlip.value = false
+    isUpdate.value = false
     calculateOffset()
 }
-const setRotateDeg = (index) => {
-    const num = 1
-    const deg = (activeIndex.value + num) * 2
-    if (index !== activeIndex.value) {
-        const rotateDeg = index > activeIndex.value ? deg : -deg;
-        return `rotate(${rotateDeg}deg)`;
-    } else {
-        return `translateY(-10%) scale(1.3)`;
+
+// 文字動畫
+const text1 = ref(null);
+const text2 = ref(null);
+
+const morphTime = 1
+const cooldownTime = 0.25
+let morph = 0
+let cooldown = cooldownTime
+let time = new Date()
+
+const doMorph = () => {
+    morph -= cooldown
+    cooldown = 0
+
+    let fraction = morph / morphTime
+
+    if (fraction > 1) {
+        cooldown = cooldownTime
+        fraction = 1
     }
+    setMorph(fraction)
+}
+const setMorph = (fraction) => {
+    const applyMorph = (ele, fraction) => {
+        ele.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`
+        ele.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`
+    }
+    applyMorph(text2.value, fraction)
+    applyMorph(text1.value, 1 - fraction)
+}
+const doCooldown = () => {
+    morph = 0;
+
+    text2.value.style.filter = ""
+    text2.value.style.opacity = "100%"
+    text1.value.style.filter = ""
+    text1.value.style.opacity = "0%"
+}
+const animate = () => {
+    requestAnimationFrame(animate)
+    let newTime = new Date();
+    let dt = (newTime - time) / 1500;
+    time = newTime;
+
+    cooldown -= dt;
+      if (cooldown <= 0 && isUpdate.value) {
+            doMorph()
+      } else if(!isUpdate.value){
+        doCooldown()
+      }
 }
 
 watch(screenWidth, () => {
@@ -171,14 +232,33 @@ watch(screenWidth, () => {
 
 onMounted(() => {
     init()
+    text1.value = document.querySelector("#text1")
+    text2.value = document.querySelector("#text2")
+    animate()
 })
 </script>
 <template>
     <div class="slider">
-        <h3 class="slider-name">
-            <span>{{ activeContent.name }}</span>
-            <span>{{ activeContent.japaneseName }}</span>
-        </h3>
+        <div class="slider-name-wrap">
+            <h3 class="slider-name" id="text1">
+                <span>{{ preContent.name }}</span>
+                <span>{{ preContent.japaneseName }}</span>
+            </h3>
+            <h3 class="slider-name" id="text2">
+                <span>{{ activeContent.name }}</span>
+                <span>{{ activeContent.japaneseName }}</span>
+            </h3>
+        </div>
+        <svg id="filters">
+            <defs>
+                <filter id="threshold">
+                    <feColorMatrix in="SourceGraphic" type="matrix" values="1 0 0 0 0
+                                            0 1 0 0 0
+                                            0 0 1 0 0
+                                            0 0 0 255 -140" />
+                </filter>
+            </defs>
+        </svg>
         <ul class="slider-list" ref="slideList" @transitionend="updateSliderPosition">
             <li 
             v-for="(slideItem,slideIndex) in cloneSlide" 
@@ -195,7 +275,7 @@ onMounted(() => {
                 >
             </li>
         </ul>
-        <div class="slider-content" :class="{show: isFlip}">
+        <div class="slider-content">
             <div class="pre" ref="pre">
                 <span>birth | {{ preContent.birth }}</span>
                 <span>height | {{ preContent.height }}</span>
@@ -218,12 +298,17 @@ onMounted(() => {
     .slider{
         position: relative;
         overflow: hidden;
+        &-name-wrap{
+            filter: url(#threshold) blur(0.6px);
+            opacity: .6;
+        }
         &-name{
             position: absolute;
             inset: 0;
             line-height: 1;
-            color: #ffffff86;
+            color: #ffffff;
             text-align: center;
+            
             span{
                 display: block;
                 &:first-child{
@@ -239,7 +324,6 @@ onMounted(() => {
             padding:10% 0 0;
             margin: 10% 0 0;
             list-style: none;
-            transition: all 1.5s;
         }
         &-item{
             flex: calc(100% / 3) 0 0;
@@ -248,7 +332,6 @@ onMounted(() => {
             margin: 0 5%;
             opacity: .5;
             transform-origin: bottom center;
-            transition: all 2s;
             &.active{
                 transform: translateY(-10%) scale(1.3);
                 opacity: 1;
@@ -295,7 +378,7 @@ onMounted(() => {
                 position: absolute;
                 top: -20px;
                 opacity: 0;
-                transition: all .8s ease-in;
+                transition: all .8s ease-in;    
                 transform: rotateX(90deg);
             }
         }
